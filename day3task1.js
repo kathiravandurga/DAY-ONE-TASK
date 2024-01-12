2c7451efa78fcfe40553cbb8ff4ff6d8c5d797a6
kathiravandurga <155103005+kathiravandurga@users.noreply.github.com>
2024-01-12 15:38:02 +0530

    Add files via upload

---
+++ day3task1.js
@@ -0,0 +1,9 @@
+let obj1 = {name:"person 1",age:5};
+let obj2 = {age:5 ,name:"person 1"};
+if(obj1.name==obj2.name && obj1.age==obj2.age){
+    console.log(`both ojb1 and obj2 are same
+    ${obj1=obj2}`)
+}
+    else{ console.log("it is not same")
+
+}
---
+++ day3task3.js
@@ -0,0 +1,15 @@
+let request= new XMLHttpRequest()
+request.open("GET","https://restcountries.com/v2/all")
+request.send()
+request.onload=function() {
+    var result= JSON.parse(request.response)
+    console.log(result)
+    for(i=0; i<result.length; i++){
+        console.log(result[i].name)
+        console.log(result[i].region)
+        console.log(result[i].flag)
+        console.log(result[i].subregion)
+
+    }
+    
+}
